e3a2298cb5275396cbde65ab1e3c041f9aa9821f
GamuWasTaken <victordiezruiz@proton.me>
2026-06-08 11:14:43 +0200

    Simple lexer implemented

--- playground/playground.paip.md
+++ playground/playground.paip.md
@@ -1,53 +1,86 @@
 
-[] : product type
+# [] : `scope` / `product`
  - A `context` is just a product of `variable`s
- - <> : sum type ? (Can be constructed with [] and any)
-
-{} : `scope`
- - global scope + 1 for each {}
+  - ? <> : sum type (Can be constructed with [] and any) (maybe suggar)
+ - Defines a scope inside of it
  - (@) refers to the current scope (the closest one)
  - (::) accesses the elements of a scope
+ - (@::name:type=value) defines a field in the current context
+ - return(@>), products return the last value, therefore putting (;) at the end makes the last element implicitly []
  - scopes inherit elements from parent scope, use cow
   - scope has access to every element in parent but modifications stay in the scope, the cannot leak
  - __builtin__
   - run: receives all the names it uses
  - the type of a scope is inferred by the names it uses
   - if we reference a name inside a scope, then, to be able to run it we need said name
+ - A scope is a list of expressions/statements
 
-`variable`s are values created like name:type
+# `variable`s are values created like name:type
  - they live in a `scope`
  - __builtin__ get & set(=)
 
-`function`s are values
+# `function`s are values
  - its type is built with (>)
  - (>) infix takes 2 `product`s returns type of function from former to later
- - `function`s as traits?
- - (.) first-ternary-infix operator, value.function value
-  - joins the 2 values in a temporary product, binds it to the function scope and runs it
+ - (.) first-ternary-infix operator, value1.function value2
+   - joins the 2 values in a temporary product, binds it to the function scope and runs it
+   - ? function.run [value1, value2]
 
-() : precedence
+# () : precedence
 
 
 ----
+products are lists of expressions, separated with (;)
+implicitly define a new scope that inherits the parent one, but uses cow -> Changes cannot escape the scope
+we can represent them with an:
+ - Array + Hashmap
+  - If the product does not have named values just use an array :)
+  
+do we want statements (aka pure side effect no value)
+One interesting thing is to have idempotent side effects (not linked to repetition)
 
-Do we need/want something to mark names so they are not looked in a context,
-- a simple name is looked in every context bottom up (current, parent, parent parent...)
-- we can cuallify a name with scope::name to only search in that scope
- - therefore @::name refers to 'name' in the current scope
+products are half indexable, with the var and ctx constructs
+does it make sense to index them by number?
+how do we do arrays? (builtin to call a product with each element of a scope)
 
-- How do we return from a scope?
- - @> value
+how do we do traits?
+what are types?
 
-- Append functions to types (types are products?)
+what about indexing function parameters, i mean, functions are products where not all the values are bound
+doing this we 'link' the parameters, meaning if you pass it to one you can pass it to the other
+
+maybe its interesting to have a convention to differenciate functions from simple products?
+
+How do we deal with single-name multiple-type vars?
+
+
+There is going to be several targets (debug, prod, ...) so we can optimize for each
+multiple types for a name
+
+do we handle variadics? (or just pass a product with all the args)
+there needs to be a type describint products
+ - we want to be able to have arities defined
 
 ----
+```python
 
-user: String = "pepe"
-print_name: [name: std::io::Print]>[] = {
- @::name.print
-}
+@::user: String = "iker";
 
-id: [x: Int]>[Int] = { @> x }
+#* the `@::name` couldnt be `name`, the @:: forces the caller to explicitly pass a name var, it wont take it from the parent context *#
+@::print_name: [@::name: write::data ; std::io::[write;stdout]]>[] = [
+ stdout.write [@::name];
+];
 
+#* name:type creates a var and adds it to the scope, var=val binds the value to the var *#
+@::id: [@::x: Int]>[Int] = [ @> x ];
 
-user.print_name
+user.print_name; # ok
+[].print_name; # not ok, name is only looked for in the provided context, and its empty
+
+3
+ .add [4]
+ .mul [5]
+ .write [@::sink=std::io::stdout]
+;
+
+```
--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,199 @@
+// use pest::Parser;
+// use pest_derive::Parser;
+
+// #[derive(Parser)]
+// #[grammar = "./paip.pest"]
+// struct PaipParser;
+
 fn main() {
-    println!("Hello, world!");
+    // let input = include_str!("../input.paip").trim();
+    // PaipParser::parse(todo!(), input);
+
+    let input = "name: type = [  \n     something.func [ @::a=3 ] ]";
+    let tokens = input.char_indices().tokens();
+    dbg!(input);
+    tokens.for_each(|t| println!("{:?}", t));
+    // TODO syntax iterator
 }
 
-/*
+#[derive(Debug, Clone, Copy)]
+pub enum Kind {
+    Err,        // An unrecognized token
+    Whitespace, // space
+    Name,       // \w+
+    Dot,        // .
+    Func,       // >
+    Sep,        // ;
+    Eq,         // =
+    Path,       // ::
+    Type,       // :
+    Ret,        // @>
+    Ctx,        // @
+    OpnBlk,     // [
+    ClsBlk,     // ]
+    OpnParens,  // (
+    ClsParens,  // )
+    EOI,        // End Of Input
+}
 
-Language elements:
-- symbols: language elements
-- names: letters lowecase, reference to a value
-- Types: letters capitalized, reference to a type
+#[derive(Debug, Clone)]
+pub struct Tokens<T>
+where
+    T: PeekN<Item = (usize, char)>,
+{
+    iter: T,
 
-Literals:
-- numbers: int and float literals
-- strings: "" anything quoted -> Char[]
-- functions: {} code between braces -> Function
--
+    tokens: Vec<Kind>,
+    positions: Vec<u32>,
+}
 
+impl<T> Tokens<T>
+where
+    T: PeekN<Item = (usize, char)>,
+{
+    fn add(&mut self, kind: Kind, start: u32) {
+        self.tokens.push(kind);
+        self.positions.push(start);
+    }
+}
 
+macro_rules! build_parser {
+    (
+        singles: [ $($single_char: literal => $single_kind: expr),+  $(,)?],
+        doubles: [
+            $($start_char: literal => [
+                $($last_char: literal => $double_kind: expr),+,
+                else => $double_else: expr
+            ]),+
+        ],
+        strings: [ $( $func:path => $func_kind:expr ),+ ]
 
-*/
+    ) => {
+    impl<T> Iterator for Tokens<T>
+    where T: PeekN<Item=(usize, char)>
+    {
+        type Item = Kind;
+
+        fn next(&mut self) -> Option<Self::Item> {
+            let (i, c) = self.iter.peek_n(0)?;
+            let tok = match c {
+                $( $single_char => {
+                        self.iter.skip_n(1);
+                        $single_kind
+                    } ),+
+                $( $start_char => match self.iter.peek_n(1) {
+                    $( Some((_, $last_char)) => {
+                            self.iter.skip_n(2);
+                            $double_kind
+                        } ),+
+                        None | Some(_) => {
+                            self.iter.skip_n(1);
+                            $double_else
+                        }
+                    } ),+
+                $( e if $func(e) => {
+                        while let Some((_, c)) = self.iter.peek_n(0)
+                            && $func(c)
+                        {
+                            self.iter.skip_n(1);
+                        }
+                        $func_kind
+                    } ),+
+
+                    _ => {
+                        self.iter.skip_n(1);
+                        Kind::Err
+                    }, // TODO maybe we can do something better with errors, other than treat them as tokens, specially once we start with the syntax part
+                };
+            self.add(tok, i as _);
+            Some(tok)
+            }
+        }
+    };
+
+}
+
+build_parser!(
+    singles: [
+        '.' => Kind::Dot,
+        '>' => Kind::Func,
+        ';' => Kind::Sep,
+        '=' => Kind::Eq,
+        '[' => Kind::OpnBlk,
+        ']' => Kind::ClsBlk,
+        '(' => Kind::OpnParens,
+        ')' => Kind::ClsParens
+    ],
+    doubles: [
+        ':' => [
+            ':' => Kind::Path,
+            else=> Kind::Type
+        ],
+        '@' => [
+            '>' => Kind::Ret,
+            else=> Kind::Ctx
+        ]
+    ],
+    strings: [
+        char::is_space => Kind::Whitespace,
+        char::is_name => Kind::Name
+    ]
+);
+
+pub trait PeekN: Iterator + Clone {
+    fn peek_n(&self, n: usize) -> Option<Self::Item> {
+        let mut tmp = (*self).clone();
+        for _ in 0..n {
+            tmp.next();
+        }
+        tmp.next()
+    }
+    fn skip_n(&mut self, n: usize) {
+        for _ in 0..n {
+            self.next();
+        }
+    }
+    fn skip_any(&mut self, p: fn(Self::Item) -> bool) {
+        while let Some(e) = self.peek_n(0) {
+            if p(e) {
+                self.skip_n(1);
+            }
+        }
+    }
+}
+impl<'a> PeekN for std::str::CharIndices<'a> {}
+impl<'a> PeekN for std::str::Chars<'a> {}
+
+pub trait LexingUtils {
+    fn is_name(self) -> bool;
+    fn is_space(self) -> bool;
+}
+
+impl LexingUtils for char {
+    fn is_name(self) -> bool {
+        self.is_alphanumeric() || self == '_'
+    }
+    fn is_space(self) -> bool {
+        self.is_whitespace()
+    }
+}
+
+pub trait ToTokens<T>
+where
+    T: PeekN<Item = (usize, char)>,
+{
+    fn tokens(self) -> Tokens<T>;
+}
+
+impl<T> ToTokens<T> for T
+where
+    T: PeekN<Item = (usize, char)>,
+{
+    fn tokens(self) -> Tokens<T> {
+        Tokens {
+            iter: self,
+            tokens: vec![],
+            positions: vec![],
+        }
+    }
+}
